0b22c780dcfe829bb9bcda6b1fbc8ec89df15034
JohsSalinas <johssalinas@wposs.com>
2022-10-01 11:35:25 -0500

    Terminé lo de la clase 29 de septiembre

--- Taller29Septiembre/src/taller29septiembre/Taller29Septiembre.java
+++ Taller29Septiembre/src/taller29septiembre/Taller29Septiembre.java
@@ -1,121 +1,149 @@
 package taller29septiembre;
-
 import java.util.Scanner;
-
 /**
- * @author jbrayan
+ * @author Johs Brayan Salinas López
  */
 public class Taller29Septiembre {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Lista P = new Lista();
+        Lista lista = new Lista();
         Scanner sc = new Scanner(System.in);
-        int op=1;
-        int bandera=0;
-        
-        P.primero = null;
-        
-        nodo r= new nodo();
-        nodo n= new nodo();
-        int opc;
+        lista.primero = null;
+        int opcion;
+        System.out.println("Practica nodos\n ¿Qué desea hacer?\n");
         do{
-            System.out.println("Agregue nodo");
-            int dato = sc.nextInt();
-            agregarNodo(P, dato);
-            System.out.println("Desea crear otro Nodo 1-Sí, 0-No");
-            opc = sc.nextInt();
-        }while(opc !=0);
-        
-        int primerPar=0, ban2=0,ultimoPrimo=0,cont=0,suma=0;
-        r=P.primero;
-        while(r!=null){
-           if(r.info%2==0 && ban2==0){
-               ultimoPrimo = r.info;
-               ban2 = 1;
-           }
-           if(r.info%2==0 && ultimoPrimo != r.info)
-               primerPar = r.info;
-           r = r.siguiente;
-        }
-        suma = primerPar + ultimoPrimo;
-        System.out.println("Primer par: "+ primerPar);
-        System.out.println("Ultimo par: "+ ultimoPrimo);
-        System.out.println("La suma de primer par y el ultimo par es : "+suma);
-        
-        r = P.primero;
-        System.out.println("Los pares de la lista son: ");
-        P.imprimirPares();
-        
-        P.SumaPrimeroUltimo();
-    }
-    
-    public static void agregarNodo(Lista lista, int x){
-            nodo n = new nodo();
-            nodo otro = new nodo();
-            n.info = x;
-            n.siguiente = null;
-            if(lista.primero == null)
-                lista.primero = n;
-            else{
-                otro = lista.primero;
-                lista.primero = n;
-                lista.primero.siguiente = otro;
+            System.out.println("\n1) Insertar nodo al principio\n" +
+                    "2) Insertar nodo al final\n" +
+                    "3) Sumar el primero con el último\n" +
+                    "4) Sumar el primer par con el ultimo par\n" +
+                    "5) Imprimir la lista\n" +
+                    "6) Imprimir los pares\n" +
+                    "7) Imprimir los primos\n" +
+                    "0) salir");
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Valor del nodo :");
+                    lista.agregarNodoPrincipio(sc.nextInt());
+                    break;
+                case 2:
+                    System.out.println("Valor del nodo :");
+                    lista.agregarNodoFinal(sc.nextInt());
+                    break;
+                case 3:
+                    lista.sumaPrimeroUltimo();
+                    break;
+                case 4:
+                    lista.sumarPrimerParUltimoPar();
+                    break;
+                case 5:
+                    lista.imprimirLista();
+                    break;
+                case 6:
+                    lista.imprimirPares();
+                    break;
+                case 7:
+                    lista.imprimirPrimos();
+                    break;
+                case 0:
+                    System.out.println("Gracias por usar el programa");
+                    break;
+                default:
+                    System.out.println("Digita un un número válido");
+                    break;
             }
-        }
-
+        }while(opcion !=0);
+    }
     public static class Lista{
     nodo primero;
-
-        public void SumaPrimeroUltimo(){
-            nodo r = new nodo();
-            r = primero;
+        public void agregarNodoPrincipio(int valor){
+            nodo temporal = new nodo();
+            nodo otro = new nodo();
+            temporal.info = valor;
+            temporal.siguiente = null;
+            if(primero == null)
+                primero = temporal;
+            else{
+                otro = primero;
+                primero = temporal;
+                primero.siguiente = otro;
+            }
+        }
+        public void agregarNodoFinal(int valor){
+            nodo nuevo = new nodo();
+            nodo temporal = new nodo();
+            nuevo.info = valor;
+            nuevo.siguiente = null;
+            if(primero == null)
+                primero = nuevo;
+            else{
+                temporal = primero;
+                while(temporal.siguiente != null)
+                    temporal = temporal.siguiente;
+                temporal.siguiente = nuevo;
+            }
+        }
+        public void sumaPrimeroUltimo(){
+            nodo temporal = new nodo();
+            temporal = primero;
             int pr=0,ul=0;
-            if(r!=null){
-                pr = r.info;
-                while (r.siguiente!=null)
-                    r = r.siguiente;
-                ul = r.info;
+            if(temporal!=null){
+                pr = temporal.info;
+                while (temporal.siguiente!=null)
+                    temporal = temporal.siguiente;
+                ul = temporal.info;
             }
             System.out.println("La suma del primero y del ultimo es: "+(pr+ul));
         }
+        public void sumarPrimerParUltimoPar(){
+            int primerPar=0, ban2=0, ultimoPrimo=0;
+            nodo temporal = new nodo();
+            temporal=primero;
+            while(temporal!=null){
+                if(temporal.info%2==0 && ban2==0){
+                    primerPar = temporal.info;
+                    ban2 = 1;
+                }
+                if(temporal.info%2==0 && ultimoPrimo != temporal.info)
+                    ultimoPrimo = temporal.info;
+                temporal = temporal.siguiente;
+            }
+            System.out.println("La suma de primer par y el ultimo par es : "+(primerPar + ultimoPrimo));
+        }
         public void imprimirLista(){
-            nodo r = new nodo();
-            r = primero;
-            while (r != null) {            
-                System.out.println(r.info);
-                r = r.siguiente;
+            nodo temporal = new nodo();
+            temporal = primero;
+            while (temporal != null) {
+                System.out.println(temporal.info);
+                temporal = temporal.siguiente;
             }
         }
-
         public void imprimirPares(){
-            nodo T = new nodo();
-            T = primero;
-
-            while(T!=null){
-                if (T.esPar())
-                    System.out.println("Es par: "+ T.info);
-            T = T.siguiente;
+            nodo temporal = new nodo();
+            temporal = primero;
+            while(temporal!=null){
+                if (temporal.esPar())
+                    System.out.println("Es par: "+ temporal.info);
+            temporal = temporal.siguiente;
+            }
+       }
+       public void imprimirPrimos(){
+            nodo temporal = new nodo();
+            temporal = primero;
+            while (temporal!=null){
+                if (temporal.esPrimo())
+                    System.out.println("Es primo: "+ temporal.info);
+                temporal = temporal.siguiente;
             }
        }
     }
     private static class nodo{
     int info;
     nodo siguiente;
-
         public Boolean esPar(){
-            Boolean R = true;
-            if(info % 2 ==0)
-                R = true;
-            else
-                R = false;
-            return R;
+            return (info % 2 == 0);
         }
         public Boolean esPrimo(){
-            Boolean R = true;
-            return  R;
+            return (info % 2 == 0 && info % 3 == 0 && info % 5 == 0 && info % 7 ==0 || (info == 1 || info ==2 || info == 3 || info ==5 || info ==7));
         }
     }
 }
